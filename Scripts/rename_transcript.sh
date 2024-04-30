6c27d49607a6f4dcd9e5489a7d9bf2626037c37e
maudvangin <157878741+maudvangin@users.noreply.github.com>
2024-04-30 15:28:12 +0200

    Delete Scripts directory

--- Scripts/rename_transcript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#PBS -N confint
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=01:00:00
-#PBS -l vmem=6gb
-
-for f in *fa ; do python "/scratch/gent/472/vsc47291/MA1_CompPlantDev/OrthoFinder/tools/primary_transcript.py" "/scratch/gent/472/vsc47291/MA1_CompPlantDev/data/Proteomes" ; done
-
